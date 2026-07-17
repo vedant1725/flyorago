e80be43ceec2793a2f5a616377730bfb543625d5
VedantPanchal <panchalvedant331@gmail.com>
2026-07-17 12:35:32 +0530

    update code

--- flyora-backend/apps/bookings/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Booking
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sender', 'traveler', 'package_name', 'weight', 'reward', 'status', 'payment_status')
-    list_filter = ('status', 'payment_status', 'created_at')
-    search_fields = ('sender__email', 'traveler__email', 'package_name')
-    ordering = ('-created_at',)
--- flyora-backend/apps/reviews/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ReviewListCreateView
-
-urlpatterns = [
-    path('', ReviewListCreateView.as_view(), name='review_list_create'),
-]
